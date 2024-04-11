97dd6abe43b7215bea918e7b1c928c0090c179c5
andrejm02 <andrejm17@gmail.com>
2024-04-11 05:18:48 +0200

    Added face culling

--- src/main.cpp
+++ src/main.cpp
@@ -162,8 +162,13 @@ int main() {
     // configure global opengl state
     // -----------------------------
     glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
 
     // build and compile shaders
     // -------------------------
